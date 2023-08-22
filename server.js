a147a1b1ba0714d42efb69bc881445ce2e1372b1
Mohamed Mmamdouh <93613974+mmdohm99@users.noreply.github.com>
2023-08-22 15:02:50 +0300

    cors headers alllowed
    Signed-off-by: Mohamed Mmamdouh <93613974+mmdohm99@users.noreply.github.com>

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const examRouter = require("./routes/examRouter.js");
 app.use(express.json());
 app.use("/exam", examRouter);
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  });
+
+  next();
+});
 app.get("*", (req, res, next) => {
   const err = new Error("sorry,this is invalid url");
   err.status = "fail";
